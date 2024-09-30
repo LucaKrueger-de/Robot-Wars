2a50495e5faf7db59b0bf77bb66460b7e5f922b4
Krüger <lucadavid.krueger@btc-ag.com>
2024-09-30 08:53:33 +0200

    sortiert

--- src/main/java/com/btcag/bootcamp/SpielBoard.java
+++ src/main/java/com/btcag/bootcamp/SpielBoard.java
@@ -1,4 +1,5 @@
 package com.btcag.bootcamp;
+
 import java.util.Random;
 import java.util.Scanner;
 
@@ -6,44 +7,30 @@ public class SpielBoard {
     public static Scanner readIn = new Scanner(System.in);
     public static Random random = new Random();
 
+    //----------------------------------------------------Intro-------------------------------------------------------------
+    public static void intro() {
 
-    public static void main(String[] args) {
-        intro();
-        String username = getName();
-        String roboter = avatar(username);
-        String enemy = enemyRobot();
-        String roboter2 = enemyAvatar(enemy);
-
-
-        drawFieldwithPlayer(roboter, username, roboter2, enemy);
-        turn(roboter, roboter2);
-    }
-
-
-    //----------------------------------------------------FieldWithPlayer-------------------------------------------------------------
-    public static void drawFieldwithPlayer(String roboter, String username, String enemy, String roboter2) {
-        System.out.println("Hier sehen Sie das Spielfeld mit Ihrem Avatar und ihrem Gegner: ");
         System.out.println();
-        System.out.println(username + " " + roboter + "                                                 " + roboter2 + " " + enemy);
-        int x = 0;
-        int y = 0;
-        while (y < 10) {
-            x = 0;
-            while (x < 15) {
-                System.out.print("[  ]");
-                x = x + 1;
-                if (y == 6 && x == 14) {
-                    System.out.print("[ " + roboter + "]");
-                    x++;
-                }
-                if (y == 6 && x == 1) {
-                    System.out.print("[ " + enemy + "]");
-                    x++;
-                }
-            }
-            System.out.println();
-            y++;
-        }
+        System.out.println("  _____   ____  ____   ____ _______  __          __     _____   _____ \n" +
+                " |  __ \\ / __ \\|  _ \\ / __ \\__   __| \\ \\        / /\\   |  __ \\ / ____|\n" +
+                " | |__) | |  | | |_) | |  | | | |     \\ \\  /\\  / /  \\  | |__) | (___  \n" +
+                " |  _  /| |  | |  _ <| |  | | | |      \\ \\/  \\/ / /\\ \\ |  _  / \\___ \\ \n" +
+                " | | \\ \\| |__| | |_) | |__| | | |       \\  /\\  / ____ \\| | \\ \\ ____) |\n" +
+                " |_|  \\_\\\\____/|____/ \\____/  |_|        \\/  \\/_/    \\_\\_|  \\_\\_____/ \n" +
+                "                                                                     ");
+        System.out.println();
+        System.out.println("MACH DICH BEREIT ZU KÄMPFEN");
+        System.out.println();
+    }
+    //----------------------------------------------------Spielername-------------------------------------------------------------
+
+    private static String getName() {
+        String username;
+        do {
+            System.out.println("(ง'̀-'́)ง Geben Sie ihren Username ein, bevor es in die Schlacht geht (ง'̀-'́)ง: ");
+            username = readIn.nextLine();
+        } while (username.length() < 3 || username.length() > 15);
+        return username;
     }
 
     //----------------------------------------------------Player-Avatar-------------------------------------------------------------
@@ -101,33 +88,6 @@ public class SpielBoard {
         return roboter;
     }
 
-    //----------------------------------------------------Intro-------------------------------------------------------------
-    public static void intro() {
-
-        System.out.println();
-        System.out.println("  _____   ____  ____   ____ _______  __          __     _____   _____ \n" +
-                " |  __ \\ / __ \\|  _ \\ / __ \\__   __| \\ \\        / /\\   |  __ \\ / ____|\n" +
-                " | |__) | |  | | |_) | |  | | | |     \\ \\  /\\  / /  \\  | |__) | (___  \n" +
-                " |  _  /| |  | |  _ <| |  | | | |      \\ \\/  \\/ / /\\ \\ |  _  / \\___ \\ \n" +
-                " | | \\ \\| |__| | |_) | |__| | | |       \\  /\\  / ____ \\| | \\ \\ ____) |\n" +
-                " |_|  \\_\\\\____/|____/ \\____/  |_|        \\/  \\/_/    \\_\\_|  \\_\\_____/ \n" +
-                "                                                                     ");
-        System.out.println();
-        System.out.println("MACH DICH BEREIT ZU KÄMPFEN");
-        System.out.println();
-    }
-
-    //----------------------------------------------------Spielername-------------------------------------------------------------
-
-    private static String getName() {
-        String username;
-        do {
-            System.out.println("(ง'̀-'́)ง Geben Sie ihren Username ein, bevor es in die Schlacht geht (ง'̀-'́)ง: ");
-            username = readIn.nextLine();
-        } while (username.length() < 3 || username.length() > 15);
-        return username;
-    }
-
     //----------------------------------------------------Enemy-Robot-------------------------------------------------------------
     public static String enemyRobot() {
         String enemy;
@@ -137,7 +97,6 @@ public class SpielBoard {
 
         return enemy;
     }
-
 
     //----------------------------------------------------Enemy-Symbol-------------------------------------------------------------
 
@@ -197,11 +156,37 @@ public class SpielBoard {
         }
         return roboter2;
     }
-//----------------------------------------------------Spielzug-------------------------------------------------------------
+
+    //----------------------------------------------------FieldWithPlayer-------------------------------------------------------------
+    public static void drawFieldwithPlayer(String roboter, String username, String enemy, String roboter2) {
+        System.out.println("Hier sehen Sie das Spielfeld mit Ihrem Avatar und ihrem Gegner: ");
+        System.out.println();
+        System.out.println(username + " " + roboter + "                                                 " + roboter2 + " " + enemy);
+        int x = 0;
+        int y = 0;
+        while (y < 10) {
+            x = 0;
+            while (x < 15) {
+                System.out.print("[   ]");
+                x = x + 1;
+                if (y == 6 && x == 14) {
+                    System.out.print("[ " + roboter + " ]");
+                    x++;
+                }
+                if (y == 6 && x == 1) {
+                    System.out.print("[ " + enemy + " ]");
+                    x++;
+                }
+            }
+            System.out.println();
+            y++;
+        }
+    }
+
+    //----------------------------------------------------Spielzug-------------------------------------------------------------
 
     public static void turn(String roboter2, String roboter) {
         System.out.println("\n\n\n");
-
 
         int fieldWidth = 15;
         int fieldHeight = 10;
@@ -280,8 +265,32 @@ public class SpielBoard {
             }
         }
     }
+
+
+    public static void main(String[] args) {
+        intro();
+        String username = getName();
+        String roboter = avatar(username);
+        String enemy = enemyRobot();
+        String roboter2 = enemyAvatar(enemy);
+
+
+        drawFieldwithPlayer(roboter, username, roboter2, enemy);
+        turn(roboter, roboter2);
+    }
 }
-//----------------------------------------------------Win-------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
